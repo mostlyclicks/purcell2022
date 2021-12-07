c45dc0e21b6ba40024824377255b4284d0ce8618
mostlyclicks <carlos@mostlyclicks.com>
2021-12-06 20:13:03 -0700

    fix duplicate prop and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import logo from './images/purcellcomm_logo.png';
 import './App.css';
 import Welcome from './components/Welcome'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="Purcell Communications Logo" className="min-h-25" />
-        
-        
+        <img src={logo} className="App-logo min-h-25" alt="Purcell Communications Logo" />
       </header>
 
       <Welcome />
-        <p>
-          <strong>Ruth Purcell</strong><br/>
-          <a href="mailto:ruthpurcell@tds.net">ruthpurcell@tds.net</a><br/>
-          5602 Barton Road<br/>
-          Madison, WI 53711
-        </p>
-
-        <p>Phone: <a href="tel:+16094425721">609.442.5721</a></p>
-        <small>Copyright © 2021 Ruth Purcell Communications</small>
+      <Footer />
+      
     </div>
   );
 }
