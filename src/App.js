210d73bc2d2c919488f77220453aae9f1398cf81
mostlyclicks <carlos@mostlyclicks.com>
2021-12-06 19:22:04 -0700

    add address and logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import logo from './images/purcellcomm_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Purcell Communications Logo" />
+        <h2>Website coming soon!</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Ruth Purcell</strong><br/>
+          <a href="mailto:ruthpurcell@tds.net">ruthpurcell@tds.net</a><br/>
+          5602 Barton Road<br/>
+          Madison, WI 53711
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>Phone: <a href="tel:+16094425721">609.442.5721</a></p>
+        <small>Copyright © 2021 Ruth Purcell Communications</small>
+        
       </header>
     </div>
   );
