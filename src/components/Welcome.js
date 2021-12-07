98ec2c850d08ea786d2f4b0c2ecbe0cdb67e3b3c
mostlyclicks <carlos@mostlyclicks.com>
2021-12-06 20:45:57 -0700

    center body vertically

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Welcome = () => {
   return (
-    <div className="p-8 container mx-auto flex-grow">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Purcell Communications</h1>
+    <div className="p-8 md:container mx-auto flex-grow lg:px-48 flex">
+    <div className="self-center">
+      <h1 className="text-3xl font-bold mb-4 ">Welcome to Purcell Communications</h1>
 
       <p className="text-base text-left mb-4">Think of us as your “general contractor” for all of your marketing communication needs.</p>
 
@@ -12,6 +13,7 @@ const Welcome = () => {
       <h2 className="text-2xl font-bold mb-4">Why use us?</h2>
 
       <p className="text-base text-left mb-4">We take care of the marketing — you run your business — Good marketing requires attention to detail — and professionals who understand your goals. You can rely on Purcell Communications to take the lead and direct your marketing efforts — keeping you in the loop on the decisions you need to make to successfully grow your business. We’re economical — Our services are a la carte — there are no retainers and there is no incentive for us to offer you more than what you need.</p>
+      </div>
 
     </div>
   )
